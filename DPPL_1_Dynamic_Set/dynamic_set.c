873452b11b0dd3ec5f27e4cc4a736e32e4e7ba0d
Piyush Moolchandani <42408687+mepiyush1@users.noreply.github.com>
2019-01-23 00:25:57 +0530

    Union function added.
    All operations of Static set and Dynamic set + Union supported now

--- DPPL_1_Dynamic_Set/dynamic_set.c
+++ DPPL_1_Dynamic_Set/dynamic_set.c
@@ -85,7 +85,7 @@ int size(node * set)
     return count;
 }
 
-void removekey(node ** set, int key)
+void remove_(node ** set, int key)
 {
     node * temp_root = *set;
     node * backward_pointer;
@@ -114,9 +114,113 @@ void removekey(node ** set, int key)
     return;
 }
 
+bool is_empty(node * S)
+{
+    if (S == NULL)
+        return true;
+    return false;
+}
+
+bool is_element_of(int key, node * set)
+{
+    if (is_empty(set))
+        return false;
+    node * temp_root = set;
+    while(temp_root -> key != key)
+    {
+        if (temp_root -> key == key)
+            return true;
+        if (temp_root -> next == NULL)
+            return false;
+        temp_root = temp_root -> next;
+    }
+}
+
+node * build(int a[], int n)
+{
+    node * new_root = create();
+    for (int i = 1; i <= n; i++)
+    {
+        add(&new_root, a[n - i]);
+    }
+    return new_root;
+}
+
+// Union Function: returns the union of sets S and T
+node * union_(node * S, node * T)
+{
+    // Return NULL if both the sets are empty.
+    if (is_empty(S) && is_empty(T))
+    {
+        printf("%s", "\nBoth the sets given for union operation are empty.");
+        return NULL;
+    }
+
+    // If one of the set is empty return other set.
+    else if (is_empty(S))
+        return T;
+    else if (is_empty(T))
+        return S;
+
+    // If none of the sets is empty then
+    else
+    {
+            // Initiate set for union operation
+            node * or_set = create();
+
+            for (node * temp = S; temp != NULL; temp = temp -> next)
+            {
+                // If not already not present in the set, insert the element.
+                if (!is_element_of(temp -> key, or_set))
+                    add(&or_set, temp -> key);
+            }
+
+            for (node * temp = T; temp != NULL; temp = temp -> next)
+            {
+                // If not already not present in the set, insert the element.
+                if (!is_element_of(temp -> key, or_set))
+                    add(&or_set, temp -> key);
+            }
+
+            // Return union of two sets.
+            return or_set;
+    }
+}
+
+// Intersection Function: returns the intersection of sets S and T
+node * intersection(node * S, node * T)
+{
+    // Return NULL if both the sets are empty.
+    if (is_empty(S) && is_empty(T))
+    {
+        printf("%s", "\nBoth the sets given for Intersection operation are empty.");
+        return NULL;
+    }
+
+    // If one of the set is empty return empty set.
+    else if (is_empty(S))
+        return NULL;
+    else if (is_empty(T))
+        return NULL;
+    else
+    {
+            node * and_set = create();
+            for (node * temp = S; temp != NULL; temp = temp -> next)
+            {
+                // If also present in the T, insert the element.
+                if (is_element_of(temp -> key, T))
+                    add(&and_set, temp -> key);
+            }
+
+            // Return union of two sets.
+            return and_set;
+    }
+}
+
+
 int main()
 {
-    printf("%s", "Output:");
+   /* printf("%s", "Output:");
     node * set1 = create();
     add(&set1, 5);
     add(&set1, 4);
@@ -135,4 +239,17 @@ int main()
     removekey(&set1, 7);
     removekey(&set1, 5);
     enumerate(set1);
+    printf("\n%d", is_element_of(5, set1));
+    printf("\n%d", is_element_of(33, set1));
+    printf("\n%d", is_element_of(9, set1));
+    printf("\n%d", is_element_of(4, set1));
+    int a[9] = {1, 3, 5, 8, 3, 6, 27, 89, 52};
+    node * new_set = build(a, 9);
+    enumerate(new_set);
+    int b[5] = {1, 5, 8, 2, 5};
+    node * new_set2 = build(b, 5);
+    enumerate(new_set2);
+    node * union_set = union_(new_set, new_set2);
+    enumerate(union_set);*/
+
 }
