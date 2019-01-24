bd38366cf4b5d0fc3e009146a0d93c2d31584e3c
Piyush Moolchandani <42408687+mepiyush1@users.noreply.github.com>
2019-01-24 18:11:09 +0530

    Changed difference function.
    Modification in difference function, is_element_of function and addition of comments in whole program.

--- DPPL_1_Dynamic_Set/dynamic_set.c
+++ DPPL_1_Dynamic_Set/dynamic_set.c
@@ -2,27 +2,37 @@
 #include<stdlib.h>
 #include<stdbool.h>
 
+// Create a structure data structure that stores data and a link to next node.
 typedef struct node
 {
     int key;
     struct node * next;
 }node;
 
+// create_node(key) function: function to return a node with key value given and next pointer with NULL value.
 node * create_node(int key)
 {
+    // Allocate memory for the node.
     node * new_node = (node *)malloc(sizeof(node));
+
+    // Assign values to key and next
     new_node -> key = key;
     new_node -> next = NULL;
+
     return new_node;
 }
 
+// create() function: Creates a new, initially empty set structure.
 node * create()
 {
-   return NULL;
+    // As set is empty, return NULL.
+    return NULL;
 }
 
+// add(&set, key) function: adds the element key to &set, if it is not present already.
 void add(node ** set, int key)
 {
+    // If set is initially empty.
     if (*set == NULL)
     {
         *set = create_node(key);
@@ -32,8 +42,10 @@ void add(node ** set, int key)
     bool presence_flag = false;
     node * temp_root;
 
+    // Traverse through the list, check if it is already present in the list.
     for (temp_root = *set; temp_root != NULL; temp_root = temp_root -> next)
     {
+        // if key found, return.
         if (temp_root -> key == key)
         {
             presence_flag = true;
@@ -41,26 +53,31 @@ void add(node ** set, int key)
         }
     }
 
+    // If key wasn't there, add the key.
     if (!presence_flag)
     {
         node * new_node = create_node(key);
         new_node -> next = *set;
         *set = new_node;
     }
+
     return;
 }
 
+// enumerate(set) function: prints all the elements of the list in some arbitrary order.
 void enumerate(node * set)
 {
     node * temp_root = set;
     int count = 0;
 
+    // If set is empty, return.
     if (temp_root == NULL)
     {
-        printf("\ns", "Set is empty.");
+        printf("\n%s", "Set is empty.");
         return;
     }
 
+    // print the elements
     printf("%s", "\nElements in the set are: ");
     for (temp_root = set; temp_root != NULL; temp_root = temp_root -> next)
     {
@@ -68,27 +85,34 @@ void enumerate(node * set)
         count ++;
     }
 
+    // print total key count.
     printf("\nTotal keys present: %d", count);
     return;
 }
 
+// size(set) function: returns the number of elements in set.
 int size(node * set)
 {
+    // If set is empty, return 0.
     if (set == NULL)
     {
         return 0;
     }
     int count = 0;
+
+    // else return number of elements counted by traversing through the list.
     for (node * temp_root = set; temp_root != NULL; temp_root = temp_root -> next)
         count++;
     return count;
 }
 
+// remove_(&set, key) function: removes the elements key from &set, if it is present.
 void remove_(node ** set, int key)
 {
     node * temp_root = *set;
     node * backward_pointer;
 
+    // If key if first element, remove it.
     if (temp_root != NULL && temp_root -> key == key)
     {
         *set = temp_root -> next;
@@ -96,23 +120,27 @@ void remove_(node ** set, int key)
         return;
     }
 
+    // Otherwise traverse through the list, and find the previous node of that key.
     while (temp_root != NULL && temp_root -> key != key)
     {
         backward_pointer = temp_root;
         temp_root = temp_root -> next;
     }
 
+    // If element not present, or set is empty, return same set.
     if (temp_root == NULL)
     {
         printf("\n%s", "Key not found in the given set.");
         return;
     }
 
+    // Delete the key.
     backward_pointer -> next = temp_root -> next;
     free(temp_root);
     return;
 }
 
+// is_empty(S) function: Checks whether the set is empty or not.
 bool is_empty(node * S)
 {
     if (S == NULL)
@@ -120,32 +148,42 @@ bool is_empty(node * S)
     return false;
 }
 
+// is_element_of(key, S) function: checks whether the value key is in S.
 int is_element_of(int key, node * set)
 {
+    // If empty set sent, return 0.
     if (is_empty(set))
         return 0;
+
+    // Traverse through set until key is found.
     node * temp_root = set;
     while(temp_root -> key != key)
     {
-        if (temp_root -> key == key)
-            return 1;
+        // If reached end of set, return 0.
         if (temp_root -> next == NULL)
             return 0;
         temp_root = temp_root -> next;
     }
+
+    // If key found return 1.
+    return 1;
 }
 
+// build(a, n) function: creates a set structure with n values sent through array a.
 node * build(int a[], int n)
 {
     node * new_root = create();
+
+    // Add elements to new set through add function that checks for repeated inputs as well.
     for (int i = 1; i <= n; i++)
     {
         add(&new_root, a[n - i]);
     }
+
     return new_root;
 }
 
-// Union Function: returns the union of sets S and T
+// union_(S, T) Function: returns the union of sets S and T
 node * union_(node * S, node * T)
 {
     // Return NULL if both the sets are empty.
@@ -186,7 +224,7 @@ node * union_(node * S, node * T)
     }
 }
 
-// Intersection Function: returns the intersection of sets S and T
+// intersection(S, T) Function: returns the intersection of sets S and T
 node * intersection(node * S, node * T)
 {
     // Return NULL if both the sets are empty.
@@ -216,45 +254,37 @@ node * intersection(node * S, node * T)
     }
 }
 
-// Difference function: return the difference of sets S and T
+// difference(S, T) function: return the difference of sets S and T
 node * difference(node * S, node * T)
 {
-    // Return NULL if both the sets are empty.
-    if (is_empty(S) && is_empty(T))
+    // Return NULL if set S is empty.
+    if (is_empty(S))
     {
         printf("%s", "\nBoth the sets given for Intersection operation are empty.");
         return NULL;
     }
 
-    // If one of the set is empty return the other.
-    else if (is_empty(S))
-        return T;
+    // If set T is empty return original S
     else if (is_empty(T))
         return S;
+
     else
     {
             // Initiate set for Difference pointer.
-            node * diff_set = create();
-            for (node * temp = S; temp != NULL; temp = temp -> next)
-            {
-                // If present in S but not in T, insert the element.
-                if (!is_element_of(temp -> key, T))
-                    add(&diff_set, temp -> key);
-            }
+        node * diff_set = create();
+        for (; S != NULL; S = S -> next)
+        {
+            // If present in S but not in T, insert the element.
+            if (!is_element_of(S -> key, T))
+                add(&diff_set, S -> key);
+        }
 
-            for (node * temp = T; temp != NULL; temp = temp -> next)
-            {
-                // If present in T but not S, insert the element.
-                if (!is_element_of(temp -> key, S))
-                    add(&diff_set, temp -> key);
-            }
-
-            // Return difference of two sets.
-            return diff_set;
+        // Return difference of two sets.
+        return diff_set;
     }
 }
 
-// Subset function: a predicate that tests whether the set S is a subset of set T
+// subset(S, T) function: a predicate that tests whether the set S is a subset of set T
 int subset(node * S, node * T)
 {
     // Traverse through subset to be checked
@@ -270,40 +300,11 @@ int subset(node * S, node * T)
 
 int main()
 {
-   /* printf("%s", "Output:");
-    node * set1 = create();
-    add(&set1, 5);
-    add(&set1, 4);
-    add(&set1, 7);
-    add(&set1, 5);
-    add(&set1, 7);
-    add(&set1, 9);
-    add(&set1, 4);
-    printf("\nSize : %d", size(set1));
-    enumerate(set1);
-    add(&set1, 33);
-    add(&set1, 27);
-    enumerate(set1);
-    removekey(&set1, 6);
-    removekey(&set1, 27);
-    removekey(&set1, 7);
-    removekey(&set1, 5);
-    enumerate(set1);
-    printf("\n%d", is_element_of(5, set1));
-    printf("\n%d", is_element_of(33, set1));
-    printf("\n%d", is_element_of(9, set1));
-    printf("\n%d", is_element_of(4, set1));
-    int a[9] = {1, 3, 5, 8, 3, 6, 27, 89, 52};
-    node * new_set = build(a, 9);
-    enumerate(new_set);
-    int b[6] = {52, 5, 8, 2, 5, 1};
-    node * new_set2 = build(b, 6);
-    enumerate(new_set2);
-    enumerate(union_(new_set, new_set2));
-    enumerate(intersection(new_set, new_set2));
-    enumerate(difference(new_set, new_set2));*/
     int a[] = {3, 5, 9, 6, 23442, 34958, 343};
     int b[] = {5, 343, 6};
     int c[] = {9, 5, 344, 6};
+    enumerate(difference(build(a, 7), build(b, 3)));
+    enumerate(difference(build(a, 7), build(c, 4)));
+    enumerate(difference(build(b, 3), build(a, 7)));
     printf("\na subset b: %d\nb subset a: %d\nc subset a: %d", subset(build(a, 7), build(b, 3)), subset(build(b, 3), build(a, 7)), subset(build(c, 4), build(a, 7)));
 }
