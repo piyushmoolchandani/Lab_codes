37c921e69665985b922e591eabed839f96d7303e
Piyush Moolchandani <42408687+piyushmoolchandani@users.noreply.github.com>
2019-03-20 01:07:17 +0530

    Fast Fourier Transformation
    fft.cpp -> only fft
    fft_.cpp -> trying multiplication of two number with fft

---
+++ DAA_L6/fft_.cpp
@@ -0,0 +1,57 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef complex<double> node; 
+
+vector<node> fft(vector<node>& eq) 
+{ 
+	int n = eq.size();
+
+	if (n == 1) 
+		return vector<node>(1, eq[0]); 
+
+	vector<node> comp_root(n/2); 
+	for (int i = 0; i < n/2; i++) 
+    { 
+		double omega = 2 * M_PI * i / n; 
+		comp_root[i] = node(cos(omega), sin(omega));
+	}
+
+	vector<node> even(n / 2), odd(n / 2); 
+	for (int i = 0; i < n / 2; i++) 
+    { 
+		even[i] = eq[i * 2];  
+		odd[i] = eq[(i * 2) + 1];
+	}
+
+	vector<node> r_even = fft(even); 
+	vector<node> r_odd = fft(odd);
+
+	vector<node> result(n); 
+
+	for (int i = 0; i < n / 2; i++) { 
+		result[i] = r_even[i] + comp_root[i] * r_odd[i]; 
+		result[i + n / 2] = r_even[i] - comp_root[i] * r_odd[i];
+	} 
+
+	return result; 
+} 
+
+int main() 
+{ 
+    int n;
+    cin >> n;
+    vector<node> eq_1(n);
+    for (int i = 0; i < n; i++)
+        cin >> eq_1[i];
+	vector<node> p_eq_1 = fft(eq_1);
+    vector<node> eq_2(n);
+    for (int i = 0; i < n; i++)
+        cin >> eq_2[i];
+	vector<node> p_eq_2 = fft(eq_2);
+
+	for (int i = 0; i < n; i++) 
+		cout << p_eq_1[i] << endl; 
+	for (int i = 0; i < n; i++) 
+		cout << p_eq_2[i] << endl; 
+        
+} 
