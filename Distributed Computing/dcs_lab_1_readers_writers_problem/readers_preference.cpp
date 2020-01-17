08232df930a65a8f64a86bf02456c779c4ad44fc
Piyush Moolchandani <42408687+piyushmoolchandani@users.noreply.github.com>
2020-01-17 11:58:53 +0530

    Update readers_preference.cpp

--- Distributed Computing/dcs_lab_1_readers_writers_problem/readers_preference.cpp
+++ Distributed Computing/dcs_lab_1_readers_writers_problem/readers_preference.cpp
@@ -1,10 +1,14 @@
 #include<iostream>
 #include<thread>
 #include<fstream>
-semaphore s;
-
+#include<semaphore>
 
 using namespace std;
+
+sem_t reader_writer_lock;
+sem_t reader_reader_lock;
+int reader_count = 0;
+int temp = 0;
 
 void reader();
 void writer();
@@ -12,17 +16,42 @@ void writer();
 int main(){
 	
 	cout << "Readers preference problem." << endl;
-	cout << "Please enter number of readers: " << endl;	
+	cout << "Please enter number of readers: " << endl;
 	int reader_count;
 	cin >> reader_count;
 	
 	semaphore s;
-	
 }
 
 void reader(){
+	/*
 	ifstream fin;
 	fin.open("text_data.txt");
 	char temp;
 	if (!fin.eof())
 		fin.read(temp);
+	*/
+	sem_wait(&reader_reader_lock);
+	reader_count += 1;
+	if (reader_count == 1){
+		sem_wait(&reader_writer_lock);
+	}
+	cout << temp << endl;
+	sem_post(&reader_reader_lock)
+	
+	sem_wait(&reader_reader_lock);
+	reader_count--;
+	if (reader_count == 0)
+		sem_post(&reader_writer_lock);
+	}
+	
+	
+		
+}
+
+void writer(){
+	sem_wait(&reader_writer_lock);
+	temp = 7;
+	puts(temp);
+	sem_post(&reader_writer_lock);
+}
