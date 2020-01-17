3aa59d57282673143098a62d038b2c06a65bae6d
Piyush Moolchandani <42408687+piyushmoolchandani@users.noreply.github.com>
2020-01-17 13:00:42 +0530

    Update readers_preference.cpp

--- Distributed Computing/dcs_lab_1_readers_writers_problem/readers_preference.cpp
+++ Distributed Computing/dcs_lab_1_readers_writers_problem/readers_preference.cpp
@@ -1,29 +1,41 @@
 #include<iostream>
 #include<thread>
 #include<fstream>
-#include<semaphore>
+#include<semaphore.h>
 
 using namespace std;
 
 sem_t reader_writer_lock;
 sem_t reader_reader_lock;
-int reader_count = 0;
-int temp = 0;
+int reader_count;
+int temp = 900;
 
-void reader();
-void writer();
+void * reader();
+void * writer();
 
 int main(){
 	
 	cout << "Readers preference problem." << endl;
 	cout << "Please enter number of readers: " << endl;
-	int reader_count;
 	cin >> reader_count;
 	
-	semaphore s;
+	sem_init(&reader_writer_lock, 0, 1);
+	sem_init(&reader_reader_lock, 0, 1);
+	pthread_t writer;
+	pthread_t readers[reader_count];
+	
+	pthread_create(&writer, NULL, writer, NULL);
+	for (int i = 0; i < reader_count; i++){
+		pthread_create(&readers[i], NULL, reader, NULL);
+	}
+	pthread_join(writer, NULL);
+	for (int i = 0; i < reader_count; i++){
+		pthread_join(readers[i], NULL);
+	}
+	return 0;
 }
 
-void reader(){
+void * reader(){
 	/*
 	ifstream fin;
 	fin.open("text_data.txt");
@@ -36,22 +48,21 @@ void reader(){
 	if (reader_count == 1){
 		sem_wait(&reader_writer_lock);
 	}
+	sem_post(&reader_reader_lock);
+	
 	cout << temp << endl;
-	sem_post(&reader_reader_lock)
 	
 	sem_wait(&reader_reader_lock);
 	reader_count--;
-	if (reader_count == 0)
+	if (reader_count == 0){
 		sem_post(&reader_writer_lock);
 	}
-	
-	
-		
+	sem_wait(&reader_reader_lock);
 }
 
-void writer(){
+void * writer(){
 	sem_wait(&reader_writer_lock);
-	temp = 7;
+	temp -= 1;
 	puts(temp);
 	sem_post(&reader_writer_lock);
 }
