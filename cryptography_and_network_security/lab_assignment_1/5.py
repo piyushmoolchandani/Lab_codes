3f3b01a1ae44619ed6d5e47c8942314a7235d8ac
Piyush Moolchandani <42408687+piyushmoolchandani@users.noreply.github.com>
2020-01-23 04:48:26 +0530

    Update 5.py

--- cryptography_and_network_security/lab_assignment_1/5.py
+++ cryptography_and_network_security/lab_assignment_1/5.py
@@ -1,18 +1,40 @@
 def gcd(a, b):
-	if (b == 0):
-		return a;
-	else:
-		return gcd(b, a % b);
-		
-		
-def inverse_pairs_ext_euc(n):
+    if (b == 0):
+        return a;
+    else:
+        return gcd(b, a % b);
 
-	mul = set()
-	for i in range(1, n):
-		if (gcd(n, i) != 1):
-			continue
-		else:
-		
+def extended_gcd(a, b):
+    r1 = a
+    r2 = b
+    s1 = 1
+    s2 = 0
+    t1 = 0
+    t2 = 1
+    while(r2 > 0):
+        q = r1 // r2
 
-n = int(input("Enter n to add. and mul. inverse pairs using ext. euclidean algo.: "))
-inverse_pairs_ext_euc(n);
+        r = r1 - q * r2
+        r1 = r2
+        r2 = r
+
+        s = s1 - q * s2
+        s1 = s2
+        s2 = s
+
+        t = t1 - q * t2
+        t1 = t2
+        t2 = t
+    return [r1, s1, t1]
+
+def inverse(n, b):
+    if gcd(n, b) == 1:
+        return [(n - b) % n, extended_gcd(n, b)[2] % n]
+    else:
+        return [n - b, "Does not exist"]
+
+print("Additive and Multiplicative inverse of b in Zn using extended euclidean algo")
+n = int(input("Enter n: "))
+b = int(input("Enter b: "))
+ans = inverse(n, b)
+print("Additive Inverse:", ans[0], ", Multiplicative inverse:", ans[1])
