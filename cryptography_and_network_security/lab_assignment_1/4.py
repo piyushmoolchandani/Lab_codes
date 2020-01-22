38108ae3edf244a99e0e7918fcbc5c9742625eb4
Piyush Moolchandani <42408687+piyushmoolchandani@users.noreply.github.com>
2020-01-23 04:32:39 +0530

    Update 4.py

--- cryptography_and_network_security/lab_assignment_1/4.py
+++ cryptography_and_network_security/lab_assignment_1/4.py
@@ -1,21 +1,29 @@
+def gcd(a, b):
+    if (b == 0):
+        return a;
+    else:
+        return gcd(b, a % b);
+    
 def inverse_pairs(n):
-	inverses = {}
-	multiplicative = set();
-	for i in range(1, n):
-		for j in range(1, n):
-			if ((i * j) % n == 1):
-				multiplicative.add((min(i, j), max(i, j)));
-	
-	additive = set();
-	for i in range(1, n):
-		for j in range(1, n):
-			if (not (i + j) % n):
-				additive.add((min(i, j), max(i, j)));
-	
-	inverses['additive_inverses'] = additive;
-	inverses['multiplicative_inverses'] = multiplicative;
-	
-	return inverses;
+    inverses = {}
+    
+    additive = set();
+    for i in range(n):
+        for j in range(i, n):
+            if (not (i + j) % n):
+                additive.add((i, j));
+    
+    multiplicative = set();
+    for i in range(1, n):
+        if gcd(n, i) == 1:
+            for j in range(i, n):
+                if ((i * j) % n == 1):
+                    multiplicative.add((i, j));
+
+    inverses['additive_inverses'] = additive;
+    inverses['multiplicative_inverses'] = multiplicative;
+
+    return inverses;
 
 n = int(input("Enter n to add. and mul. inverse pairs: "))
 print(inverse_pairs(n));
